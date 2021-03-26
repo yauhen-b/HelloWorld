9f5447bf671fc9acc831844423791233d0764156
yauhen_b <by.eugean@gmail.com>
2021-03-26 12:52:47 +0300

    Add ui text

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HelloWorld : MonoBehaviour
 {
+    public Text label;
     public int min = 1;
     [SerializeField]
     private int max = 1000;
@@ -33,6 +35,7 @@ public class HelloWorld : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
         {
+            label.text = "Конец";
             print(message:"Конец");
         }
     }
@@ -40,6 +43,7 @@ public class HelloWorld : MonoBehaviour
     private void UpdateGuess()
     {
         guess = (min + max) / 2;
+        label.text = "Твоё число равно " + guess + "?";
         print(message:"Твоё число равно " + guess + "?");
     }
 }
