d2ce68d33d67ed51e7f9b779b417aceeb6583b88
yauhen_b <by.eugean@gmail.com>
2021-03-31 11:48:48 +0300

    MagicNumbers done

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -4,20 +4,19 @@ using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Magic Numbers script
 public class HelloWorld : MonoBehaviour
 {
     public Text label;
-    public int min = 1;
-    [SerializeField]
-    private int max = 1000;
-    
-    private int guess;
+    public int min;
+    public int max;
+    public int count;
+    public int guess;
     
     // Start is called before the first frame update
     private void Start()
     {
-        print(message:"Загадай число от " + min +" до " + max);
-        UpdateGuess();
+        StartGuess();
     }
 
     // Update is called once per frame
@@ -33,17 +32,29 @@ public class HelloWorld : MonoBehaviour
             max = guess;
             UpdateGuess();
         }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGuess();
+        }
         if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return))
         {
             label.text = "Конец";
-            print(message:"Конец");
         }
     }
 
     private void UpdateGuess()
     {
         guess = (min + max) / 2;
-        label.text = "Твоё число равно " + guess + "?";
-        print(message:"Твоё число равно " + guess + "?");
+        label.text = "Твоё число равно " + guess + "?\n число попыток равно " + ++count;
+    }
+
+    private void StartGuess()
+    {
+        min = 1;
+        max = 1000;
+        guess = (min + max) / 2;
+        count = 0;
+        label.text = "Загадай число от " + min + " до " + max + 
+                     "\n Твоё число равно " + guess + "?\n число попыток равно " + ++count;
     }
 }
